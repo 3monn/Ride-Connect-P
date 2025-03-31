8d3a09b6efd4f0f5c46fa1097e1cc41807e3bcb5
3monn <omar59056@gmail.com>
2025-04-01 00:37:03 +0300

    change accept offer http method from put to patch

--- src/main/java/com/rideconnect/rideconnect/Controller/Driver_offerController.java
+++ src/main/java/com/rideconnect/rideconnect/Controller/Driver_offerController.java
@@ -2,6 +2,8 @@ package com.rideconnect.rideconnect.Controller;
 
 import com.rideconnect.rideconnect.Models.Driver_Offer;
 import com.rideconnect.rideconnect.Service.Driver_OfferService;
+
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,13 +15,14 @@ public class Driver_offerController {
 
     private final Driver_OfferService driver_offerService;
 
-
     public Driver_offerController(Driver_OfferService driver_offerService) {
         this.driver_offerService = driver_offerService;
     }
-    
-    @PostMapping("/accept_offer")
-    public Driver_Offer accept_offer(@RequestParam("offer_id") Integer offer_id, @RequestParam("driver_id") Integer driver_id) {
+
+    @PatchMapping("/accept_offer")
+    public Driver_Offer accept_offer(@RequestParam("offer_id") Integer offer_id,
+            @RequestParam("driver_id") Integer driver_id) {
         return driver_offerService.acceptOffer(offer_id, driver_id);
     }
+    // http://localhost:8080/api/v1/driver_offer/accept_offer?offer_id=1&driver_id=1
 }
